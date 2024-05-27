7980e402b89b79480e091cf48a042d5826dfafa2
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-05-26 17:40:29 -0700

    Status of your API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,3 +12,18 @@ from models import storage
 @app_views.route('/status', methods=['GET'])
 def get_status():
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats")
+def storage_counts():
+    '''
+        return counts of all classes in storage
+    '''
+    cls_counts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(cls_counts)
